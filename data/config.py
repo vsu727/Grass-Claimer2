2abd3dbaf4ea8c61d9d6f2d9741c0226f31551f1
Mslolita <oblikovuizapuc@gmail.com>
2024-10-28 17:58:17 +0200

    upd

--- data/config.py
+++ data/config.py
@@ -1,5 +1,6 @@
 THREADS = 1
 
+# rpc https://www.quicknode.com/login
 RPC_URL = 'https://greatest-summer-diagram.solana-mainnet.quiknode.pro/ea60cb220d957049e3e29fc07f34853f4d35313b'
 DESTINATION_ADDRESS = ''
 
